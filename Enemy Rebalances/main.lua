146d6b2c2bc0880e64b1e23e2c2ffae9e4ea8c72
piber20 <piber20@cygo.com>
2018-05-08 23:41:45 -0400

    rebalances: add blister health scaling
    simplified some of the code
    
    removed rufia's jumping ministro tweak since something like it was added to the booster

--- Enemy Rebalances/main.lua
+++ Enemy Rebalances/main.lua
@@ -3,16 +3,39 @@ EnemyRebalanceMod = RegisterMod("Enemy Rebalances", 1)
 function EnemyRebalanceMod:onChargerUpdate(entity)
 	local frameCount = entity.FrameCount
 	if frameCount <= 1 then
-		if entity.Variant == 1 then
-			if (entity.SpawnerType == EntityType.ENTITY_HIVE and entity.SpawnerVariant == 1) or (entity.SpawnerType == EntityType.ENTITY_BOOMFLY and entity.SpawnerVariant == 2) then
-				entity.MaxHitPoints = 10
-				entity.HitPoints = 10
-			else
-				entity.MaxHitPoints = 20
-				entity.HitPoints = 20
+		if entity.Variant == 1 then --drowned chargers
+			local healthToSet = 20 --force their health to the same as a normal charger's
+			
+			if entity.SpawnerType > 0 then --lower their health to half a charger's if something spawned them (like a drowned hive)
+				healthToSet = 10
 			end
+			
+			--set the health
+			entity.MaxHitPoints = healthToSet
+			entity.HitPoints = healthToSet
 		end
 	end
 end
 EnemyRebalanceMod:AddCallback(ModCallbacks.MC_NPC_UPDATE, EnemyRebalanceMod.onChargerUpdate, EntityType.ENTITY_CHARGER)
 EnemyRebalanceMod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, EnemyRebalanceMod.onChargerUpdate, EntityType.ENTITY_CHARGER)
+
+function EnemyRebalanceMod:onBlisterUpdate(entity)
+	local frameCount = entity.FrameCount
+	if frameCount <= 1 then
+		if entity.Variant == 0 then --normal blisters
+			local baseHealth = 16 --lower blister's health and give them stagehp
+			local fakeStageHP = 4
+			
+			--do stagehp math
+			local level = Game():GetLevel()
+			local stage = level:GetStage()
+			healthToSet = baseHealth + (fakeStageHP * stage)
+			
+			--set the health
+			entity.MaxHitPoints = healthToSet
+			entity.HitPoints = healthToSet
+		end
+	end
+end
+EnemyRebalanceMod:AddCallback(ModCallbacks.MC_NPC_UPDATE, EnemyRebalanceMod.onBlisterUpdate, EntityType.ENTITY_BLISTER)
+EnemyRebalanceMod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, EnemyRebalanceMod.onBlisterUpdate, EntityType.ENTITY_BLISTER)
