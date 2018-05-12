3f78f28198535514d5c8ba094d879c9aa5f9aa0d
piber20 <piber20@cygo.com>
2018-05-12 09:46:00 -0400

    remove blister stage hp

--- Monster Tweaks/main.lua
+++ Monster Tweaks/main.lua
@@ -19,27 +19,6 @@ end
 EnemyRebalanceMod:AddCallback(ModCallbacks.MC_NPC_UPDATE, EnemyRebalanceMod.onChargerUpdate, EntityType.ENTITY_CHARGER)
 EnemyRebalanceMod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, EnemyRebalanceMod.onChargerUpdate, EntityType.ENTITY_CHARGER)
 
-function EnemyRebalanceMod:onBlisterUpdate(entity)
-	local frameCount = entity.FrameCount
-	if frameCount <= 1 then
-		if entity.Variant == 0 then --normal blisters
-			local baseHealth = 16 --lower blister's health and give them stagehp
-			local fakeStageHP = 4
-			
-			--do stagehp math
-			local level = Game():GetLevel()
-			local stage = level:GetStage()
-			healthToSet = baseHealth + (fakeStageHP * stage)
-			
-			--set the health
-			entity.MaxHitPoints = healthToSet
-			entity.HitPoints = healthToSet
-		end
-	end
-end
-EnemyRebalanceMod:AddCallback(ModCallbacks.MC_NPC_UPDATE, EnemyRebalanceMod.onBlisterUpdate, EntityType.ENTITY_BLISTER)
-EnemyRebalanceMod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, EnemyRebalanceMod.onBlisterUpdate, EntityType.ENTITY_BLISTER)
-
 function EnemyRebalanceMod:onTearProjectileUpdate(projectile)
 	local frameCount = projectile.FrameCount
 	if frameCount == 2 then
